55ee40801cc4aa35830810395f17b5db437b6594
Zachary Mitchell <masspike84@yahoo.com>
2024-09-10 14:56:00 -0400

    api complete

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ProductsService } from '../services/products.service';
+import { Products } from '../../types';
 
 @Component({
     selector: 'app-home',
@@ -7,4 +9,17 @@ import { Component } from '@angular/core';
     templateUrl: './home.component.html',
     styleUrl: './home.component.scss',
 })
-export class HomeComponent {}
+export class HomeComponent {
+    constructor(private productsService: ProductsService) {}
+
+    ngOnInit() {
+        this.productsService
+            .getProducts('https://localhost:3000/clothes', {
+                page: 0,
+                perPage: 5,
+            })
+            .subscribe((products: Products) => {
+                console.log(products.items);
+            });
+    }
+}
